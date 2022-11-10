5be5eac01d3750bb80c0f44a33b69f5afaf53ec2
gingrase <gingrase@gmail.com>
2022-11-09 23:31:51 -0500

    support keyboard entries

--- script.js
+++ script.js
@@ -63,73 +63,124 @@ function resetAll() {
     digitToComeDecimals = false;
 }
 
+function numberEntered (id) {
+    if (operatorWasEnteredLast) {
+        operand2 = updateOperand(operand2, id);
+        updateScreen(operand2);
+    } else if (equalWasPressedLast) {
+        operand1 = 0;
+        equalWasPressedLast = false;
+        operand1 = updateOperand(operand1, id);
+        updateScreen(operand1);
+    } else {
+        operand1 = updateOperand(operand1, id);
+        updateScreen(operand1);
+    }
+}
+
+function operatorEntered (id) {
+    if (operatorWasEnteredLast) {
+        operand1 = calculate (operand1, operand2, operator);
+        updateScreen(operand1);
+        operand2 = 0;
+    }
+    operator = id;
+    operatorWasEnteredLast = true;
+    equalWasPressedLast = false;
+    digitToComeDecimals = false;
+}
+
+function equalEntered () {
+    if (operand2) {
+        operand1 = calculate (operand1, operand2, operator);
+        updateScreen(operand1);
+        operand2 = 0;
+    } 
+    operatorWasEnteredLast = false;
+    equalWasPressedLast = true;
+    digitToComeDecimals = false;
+}
+
+function deleteEntered () {
+    if (operatorWasEnteredLast) {
+        operand2 = Number(operand2.toString(10).slice(0, -1));
+        updateScreen(operand2);
+    } else {
+        operand1 = Number(operand1.toString(10).slice(0, -1));
+        updateScreen(operand1);
+    }
+}
+
+function clearEntered () {
+    if (operatorWasEnteredLast) {
+        operand2 = 0;
+        updateScreen(operand2);
+    } else {
+        operand1 = 0;
+        updateScreen(operand1);
+    }
+}
+
+function negativeEntered () {
+    if (operatorWasEnteredLast) {
+        operand2 *= -1;
+        updateScreen(operand2);
+    } else {
+        operand1 *= -1;
+        updateScreen(operand1);
+    }
+}
+
 function buttonReaction () {
     if (!isNaN(this.id)) {
-        if (operatorWasEnteredLast) {
-            operand2 = updateOperand(operand2, this.id);
-            updateScreen(operand2);
-        } else if (equalWasPressedLast) {
-            operand1 = 0;
-            equalWasPressedLast = false;
-            operand1 = updateOperand(operand1, this.id);
-            updateScreen(operand1);
-        } else {
-            operand1 = updateOperand(operand1, this.id);
-            updateScreen(operand1);
-        }
+        numberEntered(this.id);
     } else if (this.id == '.') {
         digitToComeDecimals = true;
     } else if (['+', '-', 'x', '/'].indexOf(this.id) >= 0) {
-        if (operatorWasEnteredLast) {
-            operand1 = calculate (operand1, operand2, operator);
-            updateScreen(operand1);
-            operand2 = 0;
-        }
-        operator = this.id;
-        operatorWasEnteredLast = true;
-        equalWasPressedLast = false;
-        digitToComeDecimals = false;
+        operatorEntered(this.id);
     } else if (this.id == '=') {
-        if (operand2) {
-            operand1 = calculate (operand1, operand2, operator);
-            updateScreen(operand1);
-            operand2 = 0;
-        } 
-        operatorWasEnteredLast = false;
-        equalWasPressedLast = true;
-        digitToComeDecimals = false;
+        equalEntered();
     } else if (this.id == 'delete') {
-        if (operatorWasEnteredLast) {
-            operand2 = Number(operand2.toString(10).slice(0, -1));
-            updateScreen(operand2);
-        } else {
-            operand1 = Number(operand1.toString(10).slice(0, -1));
-            updateScreen(operand1);
-        }
+        deleteEntered();
     } else if (this.id == 'negative') {
-        if (operatorWasEnteredLast) {
-            operand2 *= -1;
-            updateScreen(operand2);
-        } else {
-            operand1 *= -1;
-            updateScreen(operand1);
-        }
+        negativeEntered();
     } else if (this.id == 'clear') {
-        if (operatorWasEnteredLast) {
-            operand2 = 0;
-            updateScreen(operand2);
-        } else {
-            operand1 = 0;
-            updateScreen(operand1);
-        }
+        clearEntered();        
     } else if (this.id == 'reset') {
         resetAll();
         updateScreen('0');
     }
 }
 
+function keyReaction (key) {  
+    if (!isNaN(key)) {
+        numberEntered(key);
+    } else if (key == '.') {
+        digitToComeDecimals = true;
+    } else if (['+', '-', 'x', '/'].indexOf(key) >= 0) {
+        operatorEntered(key);
+    } else if (key == '=') { 
+        equalEntered();
+    } else if (key == 'd') { // Delete
+        deleteEntered();
+    } else if (key == 'n') { // Negate
+        negativeEntered(); 
+    } else if (key == 'c') { // Clear
+        clearEntered();        
+    } else if (key == 'r') { // Reset
+        resetAll();
+        updateScreen('0');
+    }
+}
+
+
 // INITIALIZATION
-let operand1, operand2, operator, digitToComeDecimals, operatorWasEnteredLast, equalWasPressedLast;
+let operand1;
+let operand2;
+let operator;
+let digitToComeDecimals;
+let operatorWasEnteredLast;
+let equalWasPressedLast;
 resetAll();
 
 // BUTTONS ACTIVATION
@@ -137,3 +188,7 @@ const buttons = document.querySelectorAll("button");
 for (let button of buttons) {
     button.addEventListener("click", buttonReaction);
 }
+
+// KEY ACTIVATION
+window.addEventListener("keypress", (event) => {keyReaction(event.key);}, true);
+  
